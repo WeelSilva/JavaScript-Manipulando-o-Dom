155c98834afd1bcb442436a2c09a7ac01e3f83c3
Well Oliveira <wsilva9589@gmail.com>
2023-06-17 20:01:27 -0300

    Colacando os primeiros cdigos em JS para verificar como funciona o DOM

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", dizOi)
+
+// criar uma funçao que aparece Oi no console
+function dizOi() { 
+    console.log("Oi")
+}
+
+
