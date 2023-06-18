966c423a812524f605863e9e795e75612922830c
Well Oliveira <wsilva9589@gmail.com>
2023-06-17 22:34:29 -0300

    continuando codigo do contador de peças

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
+const controle = document.querySelectorAll("#controle-ajuste");
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent);
+    });
+});
+
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
-)
-
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
-}
-)
-
-
