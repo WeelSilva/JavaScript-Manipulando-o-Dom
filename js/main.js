716d65f9bd482f1876e3e71333872ac575b5262b
Well Oliveira <wsilva9589@gmail.com>
2023-06-18 23:24:55 -0300

    ainda sem conseguir resolver os bugs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-
-
-const controle = document.querySelectorAll("#controle-ajuste");
+const controle = document.querySelector("[data-controle]")
 
 controle.forEach( (elemento) => {
-    elemento.addEventListener("click", (evento) => {
+    elemento.addEventListener('click', (evento) => {
         manipulaDados(evento.target.textContent, evento.target.parentNode)
-    });
-});
+    })
+})
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("#.controle-contador");
+    const peca = controle.querySelector("[data-contador]")
 
-        if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1;
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
     } else {
-        peca.value = parseInt(peca.value) + 1;
+        peca.value = parseInt(peca.value) + 1
     }
 }
